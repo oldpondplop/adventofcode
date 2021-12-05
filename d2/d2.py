59ddfda6c2d2fcecd82d14e6d68a946ccf8b790b
ntixitho <76494676+ntixitho@users.noreply.github.com>
2021-12-05 19:24:05 +0200

    Update d2.py

--- d2/d2.py
+++ d2/d2.py
@@ -2,20 +2,20 @@ def part1(inp):
     depth=0
     horizontal=0
     for i in inp:
-        val = int(i[-1])
+        val=int(i[-1])
         if i.__contains__('forward'):
             horizontal+=val
         elif i.__contains__('up'):
             depth-=val
         else: depth+=val
     print(depth*horizontal)
-
+    
 def part2(inp):
     aim=0
     depth=0
     horizontal=0
     for i in inp:
-        val = int(i[-1])
+        val=int(i[-1])
         if i.__contains__('forward'):
             horizontal+=val
             depth+=(aim*val)
