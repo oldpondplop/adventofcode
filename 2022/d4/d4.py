d3e3ebba3c9d57fc55f09dfa04e4c55458525700
John Doe <johndoe@example.com>
2022-12-08 21:07:56 +0200

    aoc 2022 :christmas_tree:

---
+++ 2022/d4/d4.py
@@ -0,0 +1,15 @@
+with open('inp.txt', 'r') as f:
+    inp = f.read().strip().replace(',', '-').split('\n')
+
+p1 = 0
+p2 = 0
+for pairs in inp:
+    s1, e1, s2, e2 = map(int, pairs.split('-'))
+    set1, set2 = map(set, (range(s1, e1 + 1), range(s2, e2 + 1)))
+    if set1.issubset(set2) or set2.issubset(set1):
+        p1+=1
+    if set1 & set2:
+        p1+=1
+
+print(p1)
+print(p2)
