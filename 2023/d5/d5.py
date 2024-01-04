e87fa1c3569738119362b9a8cfa477e1f23386a0
John Doe <null@mail.com>
2024-01-04 23:00:30 +0200

    aoc 2023 :christmas_tree:

--- 2023/d5/d5.py
+++ 2023/d5/d5.py
@@ -1,8 +1,6 @@
-from pathlib import Path
-from collections import defaultdict
-wd = Path(__file__).parent
+from collections import deque
 
-with open(wd / 'inp.txt','r') as f:
+with open('inp.txt','r') as f:
   inp = f.read().strip().split('\n\n')
 
 def p1():
@@ -15,105 +13,86 @@ def p1():
 
   location = float('inf')
   for seed in seeds:
-    for maps in mappings:
-      for s, e, d in maps:
+    for mapping in mappings:
+      for s, e, d in mapping:
         if s <= seed < e:
           seed += d
           break
-  
-    if seed < location:
-      location = seed
-  
+    location = min(seed, location)
   return location
 
-# print(p1())
 
-
-"""
-seed-to-soil map: source-destination
-50 98 2 -> dest, source, lenght (source 98: dest 50) [50, 51] -> [98, 99] (seed 98-> soil 50) | list(range(dest, dest+lenght)
-52 50 48 -> dest, source, lenght (source 50: dest 52) [50, 51 ... 96, 97] -> [52, 53 ..., 98, 99] (seed 53->soil 55)
-"""
-
-inp = """
-seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4
-""".strip().split('\n\n')
-
-
-seeds = list(map(int, inp[0].split(' ', 1)[1].split()))
-seeds = list(zip(seeds[::2], seeds[1::2]))
-print(seeds)
-mappings = []
-for section in inp[1:]:
-  values = [list(map(int, line.split())) for line in section.split('\n')[1:]]
-  _map = [(src, src+lenght, dst-src) for dst, src, lenght in values]
-  mappings.append(_map)
-
-
-seed_mapping = defaultdict(lambda: defaultdict(lambda: None))
-section_mapping = {
-  0: 'soil',
-  1: 'fertilizer',
-  2: 'water',
-  3: 'light',
-  4: 'temperature',
-  5: 'humidity',
-  6: 'location'
-}
-
-location = float('inf')
-for ss, l in seeds:
-  for seed in range(ss, ss+l):
-    k = seed
-    for i, maps in enumerate(mappings):
-      for s, e, d in maps:
-        if s <= seed < e:
-          seed += d
-          seed_mapping[k][section_mapping[i]] = seed
-          seed_mapping[k][7] = range(s, s + e), maps
-          break
-      else:
-        seed_mapping[k][section_mapping[i]] = seed
-        seed_mapping[k][7] = maps
+def p2():
+  def _calculate_overlap(seed_interval, mapping_interval):
+    """
+    seed_interval:    |---------| (s1, e1)
+    mapping_interval: |---------| (s2, e2, d)
     
-    if seed < location:
-      location = seed
+    1. Complete:
+      |-----|   (s1, e1)
+    |---------| (s2, e2)
+    
+    2. Inner:
+    |---------| (s1, e1)
+      |-----|   (s2, e2)
+    
+    3. Right:
+    |-------|   (s1, e1)
+      |-------| (s2, e2)
+    
+    4. Left:
+      |------|  (s1, e1)
+    |------|    (s2, e2)
+    """
+    s1, e1 = seed_interval
+    s2, e2, d = mapping_interval
 
-for seed, values in seed_mapping.items():
-  print(f"Seed {seed}, soil {values['soil']}, fertilizer {values['fertilizer']}, water {values['water']}, light {values['light']}, temperature {values['temperature']}, humidity {values['humidity']}, location {values['location']} {values[7]}")
+    is_left_overlap = (s2 <= s1 < e2)
+    is_right_overlap = (s2 < e1 <= e2)
+    is_inner_overlap = (s1 < s2 and e1 > e2)
 
-print(location)
+    overlapping, non_overlapping = [], []
+    if is_left_overlap and is_right_overlap:  # Complete
+      overlapping.append((s1 + d, e1 + d))
+    elif is_inner_overlap:  # Inner
+      overlapping.append((s2 + d, e2 + d))
+      non_overlapping.append((s1, s2))
+      non_overlapping.append((e2, e1))
+    elif is_right_overlap:  # Right
+      overlapping.append((s2 + d, e1 + d))
+      non_overlapping.append((s1, s2))
+    elif is_left_overlap:  # Left
+      overlapping.append((s1 + d, e2 + d))
+      non_overlapping.append((e2, e1))
+    return overlapping, non_overlapping
+  
+  seeds = list(map(int, inp[0].split(' ', 1)[1].split()))
+  seed_ranges = deque([(start, start + length) for start, length in zip(seeds[::2], seeds[1::2])])
+  mappings = []
+  for section in inp[1:]:
+    values = [list(map(int, line.split())) for line in section.split('\n')[1:]]
+    _map = [(src, src+lenght, dst-src) for dst, src, lenght in values]
+    mappings.append(_map)
+  
+  for mapping in mappings:
+    new_seed_ranges = deque()
+    
+    while seed_ranges:
+      seed_range = seed_ranges.popleft()
+      
+      for s, e, d in mapping:
+        overlapping, remaining = _calculate_overlap(seed_range, (s, e, d))
+        if overlapping: # intersection
+          new_seed_ranges.extend(overlapping)
+          if remaining: # remaining parts to check against the next mapping
+            seed_ranges.extend(remaining)
+          break
+      else: # no overlapping
+        new_seed_ranges.append(seed_range)
+        
+    seed_ranges = new_seed_ranges
+  
+  return min(seed_ranges, key=lambda x: x[0])[0]
 
-
+print(p1())
+print(p2())
